b4dccb91855f40c424381958aa0986cc7f62f6ca
neocarto <nicolas.lambert@cnrs.fr>
2025-03-13 17:45:41 +0100

    quantize

--- src/simplify.js
+++ src/simplify.js
@@ -1,15 +1,18 @@
 import { topology } from "topojson-server";
-import { feature } from "topojson-client";
+import { feature, quantize as quantiz } from "topojson-client";
 import { presimplify, quantile, simplify as simple } from "topojson-simplify";
 
 const topojson = Object.assign(
   {},
-  { topology, presimplify, quantile, simple, feature }
+  { topology, presimplify, quantiz, quantile, simple, feature }
 );
 
 // ajouter param mutate = false
 
-export function simplify(x, { k = undefined, arcs = 15000 } = {}) {
+export function simplify(
+  x,
+  { k = undefined, quantize = undefined, arcs = 15000 } = {}
+) {
   let data = JSON.parse(JSON.stringify(x));
 
   let topo = topojson.topology({ foo: data });
@@ -20,13 +23,15 @@ export function simplify(x, { k = undefined, arcs = 15000 } = {}) {
     k = k > 1 ? 1 : k;
   }
 
-  const v1 = simpl.arcs.flat().length;
   simpl = topojson.simple(simpl, topojson.quantile(simpl, k));
-  const v2 = simpl.arcs.flat().length;
+  if (quantize) {
+    console.log("quantize");
+    simpl = topojson.quantiz(simpl, quantize);
+  }
+
   data.features = topojson.feature(
     simpl,
     Object.keys(simpl.objects)[0]
   ).features;
-  console.log([k, v1, v2, v2 / v1]);
   return data;
 }
