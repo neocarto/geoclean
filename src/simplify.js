7c6b5f629d59a6ba94cd5e4a32e9339bc6808560
neocarto <nicolas.lambert@cnrs.fr>
2025-03-13 17:15:53 +0100

    wip simplify

--- src/simplify.js
+++ src/simplify.js
@@ -9,15 +9,24 @@ const topojson = Object.assign(
 
 // ajouter param mutate = false
 
-export function simplify(x, { k = 0.5 } = {}) {
+export function simplify(x, { k = undefined, arcs = 15000 } = {}) {
   let data = JSON.parse(JSON.stringify(x));
-  // simplification
+
   let topo = topojson.topology({ foo: data });
   let simpl = topojson.presimplify(topo);
+
+  if (k == undefined) {
+    k = arcs / simpl.arcs.flat().length;
+    k = k > 1 ? 1 : k;
+  }
+
+  const v1 = simpl.arcs.flat().length;
   simpl = topojson.simple(simpl, topojson.quantile(simpl, k));
+  const v2 = simpl.arcs.flat().length;
   data.features = topojson.feature(
     simpl,
     Object.keys(simpl.objects)[0]
   ).features;
+  console.log([k, v1, v2, v2 / v1]);
   return data;
 }
